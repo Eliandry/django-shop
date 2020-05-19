c5c8087d9266d20144b98c1a216aabf0c0bd0b78
eleandry <andreymacn@gmail.com>
2020-05-19 18:49:51 +0300

    Обновлен дизайн главной страницы .Функция скачивания списка заказов в текстовый файл

--- orders/admin.py
+++ orders/admin.py
@@ -1,17 +1,36 @@
 from django.contrib import admin
 from .models import Order, OrderItem
-
+import csv
+import datetime
+from django.http import HttpResponse
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
-
+def export_to_csv(modeladmin,request,queryset):
+    opts=modeladmin.model._meta
+    response=HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id',
                     'address', 'postal_code', 'city', 'paid',
                     'created']
-    list_filter = ['paid', 'created', ]
+    list_filter = ['paid', 'created',]
+    actions = [export_to_csv]
     inlines = [OrderItemInline]
 
 admin.site.register(Order, OrderAdmin)
--- shop/models.py
+++ shop/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
+    characteristics=models.TextField(max_length=200 , default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     buy = models.PositiveIntegerField(default=0)
